b5192002f43b5985d7e791048ba8a5f27c8d5ebf
lucianoBrd <lucien.burdet@gmail.com>
2019-01-18 15:28:45 +0100

    js checkbox

--- public/assets/js/default.js
+++ public/assets/js/default.js
@@ -1,14 +1,31 @@
-var check = document.getElementById("hosting_idService");
-var childs = check.children;
-for (var i = 0; i < childs.length ; i++)
-{
-    childs[i].classList.add("custom-control");
-    childs[i].classList.add("custom-checkbox");
-    childs[i].classList.remove("form-check");
+try{
+    var check = document.getElementById("hosting_idService");
+    var childs = check.children;
+    for (var i = 0; i < childs.length ; i++)
+    {
+        childs[i].classList.add("custom-control");
+        childs[i].classList.add("custom-checkbox");
+        childs[i].classList.remove("form-check");
 
-    childs[i].children[0].classList.add("custom-control-input");
-    childs[i].children[0].classList.remove("form-check-input");
+        childs[i].children[0].classList.add("custom-control-input");
+        childs[i].children[0].classList.remove("form-check-input");
 
-    childs[i].children[1].classList.add("custom-control-label");
-    childs[i].children[1].classList.remove("form-check-label");
-}
+        childs[i].children[1].classList.add("custom-control-label");
+        childs[i].children[1].classList.remove("form-check-label");
+    }
+} catch(error){}
+try{
+    var formCheck = document.querySelectorAll(".form-check");
+    
+    formCheck.forEach(function(div) {
+        div.classList.add("custom-control");
+        div.classList.add("custom-checkbox");
+        div.classList.remove("form-check");
+
+        div.children[0].classList.add("custom-control-input");
+        div.children[0].classList.remove("form-check-input");
+
+        div.children[1].classList.add("custom-control-label");
+        div.children[1].classList.remove("form-check-label");
+    });
+} catch(error){}
