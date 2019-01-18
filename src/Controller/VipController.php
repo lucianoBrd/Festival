2969aa233ea6cb0cf90d78e4fe9bd5747c2935fd
Emilio Maldonado <emilio.maldonado@etu.univ-lyon1.fr>
2019-01-18 09:19:27 +0100

    commit tqt

--- src/Controller/VipController.php
+++ src/Controller/VipController.php
@@ -16,4 +16,45 @@ class VipController extends AbstractController
             'controller_name' => 'VipController',
         ]);
     }
+    
+    /**
+     * @Route("/Vips/new", name="Vip_create")
+     * @Route("/Vips/{id}/edit", name="Vip_edit")
+     */
+    public function manageVips(Request $request, ObjectManager $manager, Vip $Vip = null)
+    {
+        if (!$Vip)
+        {
+            $Vip = new Vip();
+        }
+
+        $form = $this->createForm(VipType::class, $Vip);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $manager->persist($Vip);
+            $manager->flush();
+
+            return $this->redirectToRoute('Vips');
+        }
+
+        return $this->render('/Vip/manage.html.twig', [
+            'form' => $form->createView(),
+            'editMode' => $Vip->getId() != null
+        ]);
+    }
+
+     /**
+     * @Route("/Vips/{id}/delete", name="Vip_delete")
+     */
+    public function deleteVip(Vip $Vip, ObjectManager $manager)
+    {
+        $manager->remove($Vip);
+        $manager->flush();
+
+        return $this->redirectToRoute('Vips');
+    }
+
 }
