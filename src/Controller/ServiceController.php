f2c45f3f30c1a2ead443767ef9a7f12f61020353
lucianoBrd <lucien.burdet@gmail.com>
2019-01-18 08:25:52 +0100

    gestion service

--- src/Controller/ServiceController.php
+++ src/Controller/ServiceController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Service;
+use App\Form\ServiceType;
 use App\Repository\ServiceRepository;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -24,5 +27,52 @@ class ServiceController extends AbstractController
         return $this->render('service/all.html.twig', [
             'services' => $services
         ]);
+    }
+
+    /**
+     * @Route("/service/new", name="service_new")
+     * @Route("/service/{id}/edit", name="service_edit")
+     */
+    public function service(Service $service = null, Request $request, ObjectManager $manager){
+        
+        if(!$service){
+            $service = new Service();
+        }
+
+        $form = $this->createForm(ServiceType::class, $service);
+
+        $form->handleRequest($request);
+
+        $editMode = $service->getId() != null;
+
+        if($form->isSubmitted() && $form->isValid()){
+
+            $manager->persist($service);
+            $manager->flush();
+
+            
+            if(!$editMode){
+                $this->addFlash('success', 'Service créé');
+                return $this->redirectToRoute('service_all');
+            } 
+            $this->addFlash('success', 'Service modifié');
+            return $this->redirectToRoute('service_all');
+        }
+
+        return $this->render('service/edit.html.twig', [
+            'form' => $form->createView(),
+            'service' => $service,
+            'editMode' => $editMode
+        ]);
+    }
+
+    /**
+     * @Route("/service/{id}/delete", name="service_delete")
+     */
+    public function delete(Service $service = null, ObjectManager $manager){
+        $manager->remove($service);
+        $manager->flush();
+        $this->addFlash('success', 'Service supprimé');
+        return $this->redirectToRoute('service_all');
     }
 }
