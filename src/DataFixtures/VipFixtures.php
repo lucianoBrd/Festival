9a7d0f20d783a593f5235b0918650590186e97e8
Emilio Maldonado <emilio.maldonado@etu.univ-lyon1.fr>
2019-01-18 14:07:33 +0100

    ajout fixtures vip

--- src/DataFixtures/VipFixtures.php
+++ src/DataFixtures/VipFixtures.php
@@ -10,13 +10,25 @@ class VipFixtures extends Fixture
 {
    public function load(ObjectManager $manager)
    {
+    $faker = \Faker\Factory::create('fr_FR');
+    $profession = array();
+        $profession[0] = "Réalisateur";
+        $profession[1] = "Acteur";
+        $profession[2] = "Scénariste";
+        $profession[3] = "Influenceur";
+        $profession[4] = "Musicien";
+        $profession[5] = "Photographe";
+        $profession[6] = "Média";
+        $profession[7] = "Compagnon";
+        $profession[8] = "Membre d'équipe";
+    for($i=0; $i <= 8 ; $i++){
        $vip = new Vip();
-       $vip->setName("Paulo")
-           ->setProfession("Realisateur")
-           ->setJury(true)
-           ->setInvited(true);
+       $vip->setName($faker->name())
+           ->setProfession($profession[$i])
+           ->setJury($faker->boolean)
+           ->setInvited($faker->boolean);
        $manager->persist($vip);
-
+    }
        $manager->flush();
    }
 }
